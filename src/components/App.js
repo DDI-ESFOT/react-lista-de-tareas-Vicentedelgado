134927120e74a2764604042bd3bf24489340b8b8
Vicente <vicente.delgado@epn.edu.ec>
2021-02-09 07:33:25 -0500

    lista completas eliminar

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import React from "react";
 import '../Styles/App.css';
 import UserList from "./UserList";
 import Counter from "./counter";
-
+import TodoList from './TodoList';
 
 function App({users}) {
   return (
     <>
         <Counter/>
         <UserList users={users}/>
+        <TodoList />
 
 
     </>
